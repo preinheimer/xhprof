5c715b575784a413b06e4b62cd78f8407b27fefa
Stéphane Goetz <onigoetz@onigoetz.ch>
2012-09-21 23:51:30 +0200

    Table Formatter / Sorter

--- xhprof_lib/utils/common.php
+++ xhprof_lib/utils/common.php
@@ -11,9 +11,15 @@ function displayRuns($resultSet, $title = "")
         $url = urlencode($row['url']);
         $html['url'] = htmlentities($row['url'], ENT_QUOTES, 'UTF-8');
         $html['c_url'] = htmlentities($row['c_url'], ENT_QUOTES, 'UTF-8');
-        $date = strtotime($row['timestamp']);
-        $date = date('M d H:i:s', $date);
-        echo "\t<tr><td><a href=\"?run={$row['id']}\">$date</a><br /><span class=\"runid\">{$row['id']}</span></td><td>{$row['cpu']}</td><td>{$row['wt']}</td><td>{$row['pmu']}</td><td><a href=\"?geturl={$url}\">{$html['url']}</a></td><td><a href=\"?getcurl={$c_url}\">{$html['c_url']}</a></td></tr>\n";
+        $date = date('M d H:i:s', $row['timestamp']);
+        echo "\t<tr>";
+        echo "\t\t<td raw=\"".$row['timestamp']."\"><a href=\"?run={$row['id']}\">$date</a><br /><span class=\"runid\">{$row['id']}</span></td>";
+        echo "\t\t<td>{$row['cpu']}</td>";
+        echo "\t\t<td raw=\"".$row['wt']."\">".usecToSize($row['wt'])."</td>";
+        echo "\t\t<td raw=\"".$row['pmu']."\">".bytesToSize($row['pmu'])."</td>";
+        echo "\t\t<td><a href=\"?geturl={$url}\">{$html['url']}</a></td>";
+        echo "\t\t<td><a href=\"?getcurl={$c_url}\">{$html['c_url']}</a></td>";
+        echo "\t</tr>\n";
     }
     echo "</tbody>\n";
     echo "</table>\n";
@@ -21,7 +27,7 @@ function displayRuns($resultSet, $title = "")
 <script type="text/javascript">
 $(document).ready(function() 
     { 
-        $("#box-table-a").tablesorter( {sortList: []} ); 
+        $("#box-table-a").tablesorter( {sortList: [], textExtraction: rawextractor} );
     } 
 );
 </script>
