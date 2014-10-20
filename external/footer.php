9e06df6f37cadb255c865d539127da8b91c8daa6
Bryan Latten <latten@adobe.com>
2014-10-20 15:49:02 -0400

    Footer: removing hardcode links to $_GET + $_POST

--- external/footer.php
+++ external/footer.php
@@ -12,6 +12,6 @@ if (extension_loaded('xhprof') && $_xhprof['doprofile'] === true) {
     {
         // url to the XHProf UI libraries (change the host name and path)
         $profiler_url = sprintf($_xhprof['url'].'/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
-        echo '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>';    
+        //echo '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>';
     }
 }
