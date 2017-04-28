8309ea252d143d23d4e067221abe3356739792bc
Michael Butler <mbutler@tumblr.com>
2017-04-28 11:38:14 -0400

    Run phpcbf on external/ too

--- external/footer.php
+++ external/footer.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('XHPROF_LIB_ROOT')) {
-  define('XHPROF_LIB_ROOT', dirname(dirname(__FILE__)) . '/xhprof_lib');
+    define('XHPROF_LIB_ROOT', dirname(dirname(__FILE__)) . '/xhprof_lib');
 }
 
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
@@ -12,8 +12,7 @@ if ($_xhprof['ext_name'] && $_xhprof['doprofile'] === true) {
     $xhprof_data = call_user_func($_xhprof['ext_name'].'_disable');
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace, null, $_xhprof);
-    if ($_xhprof['display'] === true && PHP_SAPI != 'cli' && !isset($isAjax))
-    {
+    if ($_xhprof['display'] === true && PHP_SAPI != 'cli' && !isset($isAjax)) {
         // url to the XHProf UI libraries (change the host name and path)
         $profiler_url = sprintf($_xhprof['url'].'/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
         echo '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>';
